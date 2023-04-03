69c9913dfa16aba800313e0a6cf0650928e3e2a0
leahtugi <leahsparkle1@gmail.com>
2023-04-03 15:21:37 +0000

    pointers and arrays

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,19 +1,27 @@
 #include "main.h"
+#include <stdio.h>
 /**
- * print_chessboard - Prints a chessboard
- * @a: The chessboard to print
+ * print_diagsums - Entry point
+ * @a: input
+ * @size: input
+ * Return: Always 0 (Success)
  */
-void print_chessboard(char (*a)[8])
+void print_diagsums(int *a, int size)
 {
-int i;
-int j;
+	int sum1, sum2, y;
 
-for (i = 0; i < 8; ++i)
-{
-for (j = 0; j < 8; ++j)
-{
-putchar(a[i][j]);
-}
-putchar('\n');
-}
+	sum1 = 0;
+	sum2 = 0;
+
+	for (y = 0; y < size; y++)
+	{
+		sum1 = sum1 + a[y * size + y];
+	}
+
+	for (y = size - 1; y >= 0; y--)
+	{
+		sum2 += a[y * size + (size - y - 1)];
+	}
+
+	printf("%d, %d\n", sum1, sum2);
 }
