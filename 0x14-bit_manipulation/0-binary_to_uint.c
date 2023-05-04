9694dd58b8c52a90d27ff2d5a68ac74631fde063
leahtugi <leahsparkle1@gmail.com>
2023-05-04 14:04:02 +0000

    converts binary to unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,24 +10,24 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int num;
-	int len, base_two;
+	int length, base_two;
 
 	if (!b)
 		return (0);
 
 	num = 0;
 
-	for (len = 0; b[len] != '\0'; len++)
+	for (length = 0; b[length] != '\0'; length++)
 		;
 
-	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
+	for (length--, base_two = 1; length >= 0; length--, base_two *= 2)
 	{
-		if (b[len] != '0' && b[len] != '1')
+		if (b[length] != '0' && b[length] != '1')
 		{
 			return (0);
 		}
 
-		if (b[len] & 1)
+		if (b[length] & 1)
 		{
 			num += base_two;
 		}
