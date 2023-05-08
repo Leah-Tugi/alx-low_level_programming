8434d81513f95650e364ef2f9d165b3e628c827d
wasanRuntime <andrewwasan@gmail.com>
2023-05-08 22:21:01 +0300

    Write a program that displays the information contained in the ELF header at the start of an ELF file.

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,75 +1,54 @@
-#include "main.h"
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
 #include <stdio.h>
 
+#define BUFFER_SIZE 1024
 
 /**
- * error_file - checks if files can be opened.
- * @file_from: file_from.
- * @file_to: file_to.
- * @argv: arguments vector.
- * Return: no return.
+ * main - Copies the content of a file to another file.
+ *
+ * @argc: The number of arguments.
+ * @argv: The arguments.
+ *
+ * Return: If successful - 0.
+ *         Otherwise - 97 if wrong number of arguments,
+ *                     98 if can't read from file_from,
+ *                     99 if can't write to file_to,
+ *                     or 100 if can't close fd.
  */
-
-void error_file(int file_from, int file_to, char *argv[])
-{
-	if (file_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-	if (file_to == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
-}
-
-/**
- * main - check the code for ALX School students.
- * @argc: number of arguments.
- * @argv: arguments vector.
- * Return: Always 0.
- */
-
 int main(int argc, char *argv[])
 {
-	int file_from, file_to, err_close;
-	ssize_t chars, wr;
-	char buf[1024];
+int fd_from, fd_to, rd, wr;
+char buffer[BUFFER_SIZE];
+mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
 
-	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
-		exit(97);
-	}
+if (argc != 3)
+dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 
-	file_from = open(argv[1], O_RDONLY);
-	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
-	error_file(file_from, file_to, argv);
+fd_from = open(argv[1], O_RDONLY);
+if (fd_from == -1)
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
 
-	chars = 1024;
-	while (chars == 1024)
-	{
-		chars = read(file_from, buf, 1024);
-		if (chars == -1)
-			error_file(-1, 0, argv);
-		wr = write(file_to, buf, chars);
-		if (wr == -1)
-			error_file(0, -1, argv);
-	}
+fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, mode);
+if (fd_to == -1)
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 
-	err_close = close(file_from);
-	if (err_close == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
-		exit(100);
-	}
+while ((rd = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+{
+wr = write(fd_to, buffer, rd);
+if (wr == -1)
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+}
 
-	err_close = close(file_to);
-	if (err_close == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
-		exit(100);
-	}
-	return (0);
+if (rd == -1)
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
+
+if (close(fd_from) == -1)
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from), exit(100);
+
+if (close(fd_to) == -1)
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to), exit(100);
+
+return (0);
 }
